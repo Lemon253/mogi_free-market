490af91579e1277563779b03e5a35f454c972259
gotouge <gotouge@gmail.com>
2025-11-13 21:03:22 +0900

    メール本文

--- src/resources/views/auth/mail.blade.php
+++ src/resources/views/auth/mail.blade.php
@@ -1,2 +1,32 @@
-<h1>Mailtrapテストメールです</h1>
-<p>このメールがMailtrapのInboxに届いていれば、設定は成功しています。</p>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>free-market</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mail.css')}}">
+    {{-- フォントの読み込み --}}
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__ttl">
+            {{-- ロゴの画像 --}}
+            <img src="{{ asset('storage/dummy/logo.svg') }}" alt="ロゴ">
+    </header>
+
+    <main>
+        {{-- 初回ログイン時のプロフィール設定画面へ --}}
+        <a href="" class="link_email">Verify Email</a>
+    </main>
+</body>
+
+</html>
+
+
+
+
+
